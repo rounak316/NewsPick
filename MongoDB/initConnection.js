db8a1b118a3aa1c8db2321f2a4bd44c5fb1f905f
rounak316 <rounak316@gmail.com>
2017-01-08 17:00:54 +0530

    Mongo

--- MongoDB/initConnection.js
+++ MongoDB/initConnection.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://ec2-54-202-250-147.us-west-2.compute.amazonaws.com/test');
+mongoose.connect('mongodb://localhost/test');
 
 var Cat = mongoose.model('Cat', { name: String });
 
