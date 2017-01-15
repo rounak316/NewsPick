6318fcb7b11b41854b04fae2e915bd817556ad24
rounak316@gmail.com <rounak316@gmail.com>
2017-01-15 22:00:59 +0530

    Windows

--- PdfToImage.sh
+++ PdfToImage.sh
@@ -36,25 +36,25 @@ if !(resetDirector $ImageOutput);then
 echoerr "Unable To Create Directory.. Check for required permissions"
 fi
 
-if gs -sDEVICE=jpeg -dTextAlphaBits=4 -r150 -o "$ImageOutput"sd/tmp%03d.png $InputDirectory > /dev/null;then
+if gs -sDEVICE=jpeg -dTextAlphaBits=4 -r150 -o "$ImageOutput"sd/tmp%03d.jpg $InputDirectory > /dev/null;then
 echo "SD Converted"
 else
 ClearShit $ImageOutput
 fi
 
-if gs -sDEVICE=jpeg -dTextAlphaBits=4 -r600 -o "$ImageOutput"fd/tmp%03d.png $InputDirectory > /dev/null;then
+if gs -sDEVICE=jpeg -dTextAlphaBits=4 -r600 -o "$ImageOutput"fd/tmp%03d.jpg $InputDirectory > /dev/null;then
 echo "FD Converted"
 else
 ClearShit $ImageOutput
 fi
 
-if gs -sDEVICE=jpeg -dTextAlphaBits=4 -r300 -o "$ImageOutput"hd/tmp%03d.png $InputDirectory > /dev/null;then
+if gs -sDEVICE=jpeg -dTextAlphaBits=4 -r300 -o "$ImageOutput"hd/tmp%03d.jpg $InputDirectory > /dev/null;then
 echo "HD Converted"
 else
 ClearShit $ImageOutput
 fi
 
-if gs -sDEVICE=jpeg -dTextAlphaBits=4 -r15 -o "$ImageOutput"thumb/tmp%03d.png $InputDirectory > /dev/null;then
+if gs -sDEVICE=jpeg -dTextAlphaBits=4 -r15 -o "$ImageOutput"thumb/tmp%03d.jpg $InputDirectory > /dev/null;then
 echo "THUMS Converted"
 else
 ClearShit $ImageOutput
