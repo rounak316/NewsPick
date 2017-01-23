ede456241433bc8759db767f5a4360460d3fccb9
rounak316@gmail.com <rounak316@gmail.com>
2017-01-24 03:14:59 +0530

    dsada

--- test.sh
+++ test.sh
@@ -40,7 +40,7 @@ if output=$(identify -format "%[fx:w]x%[fx:h]" pic.jpg); then
     CMD='convert -quiet -size '$output' xc:white -stroke none  -strokewidth 0 -fill "rgba( 0, 0, 0 , 1 )" '$rectangleSubCommand' -fill "rgba( 255, 255, 255 , 1 )" '$PREVIOUS_TMP' "draw_rect.png"'
     $(echo $CMD > CMD)
     if output=$(sh "CMD"); then
-    	if convert -quiet pic.jpg   draw_rect.png   -compose screen    -composite -trim 'Articles/draw_rect'"$2"'.png';then
+    	if convert -quiet pic.jpg   draw_rect.png   -compose screen    -composite -trim 'Articles/draw_rect'"$2"'.jng';then
     		# echo "Yes"$convert pic.jpg   draw_rect.png   -compose screen -crop $3   -composite  'Articles/draw_rect'"$2"'.png'
 			
 			PREVIOUS_TMP=$rectangleSubCommand
